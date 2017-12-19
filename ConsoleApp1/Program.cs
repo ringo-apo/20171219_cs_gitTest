6ffbc08623231e5aa9998d2edd2ca99531d25262
ringo-apo <ringo1625@gmail.com>
2017-12-19 14:35:31 +0900

    2017/12/19

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("1番目の数字を入力してください");
+            string a = Console.ReadLine();
+            int b = int.Parse(a);
+
+            Console.WriteLine("2番目の数字を入力してください");
+            string c = Console.ReadLine();
+            int d = int.Parse(c);
+
+            int e = b + d;
+            string f = e.ToString();
+            Console.WriteLine("1番目と2番目の数字の合計は{0}です。",f);
+
         }
     }
 }
